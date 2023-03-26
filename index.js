9885bd31bbc42accb8ad750c8c190fbfd159f87c
David Rashid <davidmra@umich.edu>
2023-03-26 17:46:21 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(sport = "roller-skate"){
+return (`This Saturday, I want to ${sport}!`)
+
+}
+saturdayFun();
+
+function mondayWork(activity = ("go to the office")){
+     return ((`This Monday, I will ${activity}.`))
+    
+}
+
+function wrapAdjective(style = "*"){
+  
+ return function innerF(parameter = "special"){
+     return `You are ${style}${parameter}${style}!`
+ }
+
+}
